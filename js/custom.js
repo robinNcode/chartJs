f748df7fc8445fd0ea3a4885598296fa3f095cee
robinNcode <msmrobin518@gmail.com>
2021-04-30 01:59:04 +0600

    loop working

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ function getChart(id, chartType){
     var myChart = new Chart(id,{
         type: chartType,
         data:{
-            labels: ["C", "C++", "PhP", "JavaScript", "Python", "Sql"],
+            labels: ["C", "C++", "PHP", "JavaScript", "Python", "Sql"],
             datasets:[{
                 data: [15, 20, 34, 22, 12, 10],
                 backgroundColor: [
@@ -20,15 +20,12 @@ function getChart(id, chartType){
 
 var typeArr = ["bar", "pie", "line", "polarArea", "doughnut"];
 
-/*for(ptr=1; ptr<=5; ptr++){
-    var ctx = document.getElementById('myChart'+ptr).getContext('2d');
-    var chart = getChart(ctx, typeArr[ptr]);
-    
-    ctx = "";
-    chart = "";
-}*/
+for(ptr=0; ptr<=5; ptr++){
+    var ctx = document.getElementById('myChart'+(ptr+1)).getContext('2d');
+    getChart(ctx, typeArr[ptr]);
+}
 
-var ctx1 = document.getElementById('myChart1').getContext('2d');
+/*var ctx1 = document.getElementById('myChart1').getContext('2d');
 var ctx2 = document.getElementById('myChart2').getContext('2d');
 var ctx3 = document.getElementById('myChart3').getContext('2d');
 var ctx4 = document.getElementById('myChart4').getContext('2d');
@@ -38,4 +35,4 @@ var chart1 = getChart(ctx1, "bar");
 var chart2 = getChart(ctx2, "pie");
 var chart3 = getChart(ctx3, "line");
 var chart4 = getChart(ctx4, "polarArea");
-var chart5 = getChart(ctx5, "doughnut");
+var chart5 = getChart(ctx5, "doughnut");*/
